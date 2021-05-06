5b7e94c464b2f9ec3f1769fa4227368f750fd9b8
yulongfei <itlun@qq.com>
2021-05-06 19:33:44 +0800

    家里提交试一下

--- main.go
+++ main.go
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-package main
-
-/*
- * @Description:
- * @Author: longfei
- * @Date: 2021-04-10 09:26:45
- * @LastEditTime: 2021-04-25 17:44:17
- * @LastEditors: longfei
- * @FilePath: \go\main.go
- */
-
-import (
-	con "test/controller"
-
-	"github.com/gin-gonic/gin"
-)
-
-func main() {
-	//先以正常模式运行，下面去掉为调试模式。。
-	gin.SetMode(gin.ReleaseMode)
-
-	router := gin.Default()
-	router.LoadHTMLGlob("view/*")      //这里是引入模板文件
-	router.Static("/static", "static") //引入静态目录
-	router.GET("/", con.Index)         //首页进入控制器
-	router.GET("/aa", con.Index)
-	router.Run(":8000") //开启端口访问
-}
-=======
 package main
 
 /*
@@ -71,6 +41,5 @@ func main() {
 		v1.GET("/", con.AdminIndex) //管理页，现在是啥也还没有
 	}
 
-	r.Run(":8000") //开启端口访问
+	r.Run(":8000") //开启端口访问,本地再试一下提交
 }
->>>>>>> caeb3c84e7cad0ff5e4db2262258207fedf886b1
