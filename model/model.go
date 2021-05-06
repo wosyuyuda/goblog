3f124e37125ef04083987a03e74e1f73736df319
longfei <i@iq2.cn>
2021-05-06 14:36:03 +0800

    多加一个问题测试

--- model/model.go
+++ model/model.go
@@ -4,7 +4,7 @@ package model
  * @Description:
  * @Author: longfei
  * @Date: 2021-04-25 09:18:47
- * @LastEditTime: 2021-05-06 14:12:06
+ * @LastEditTime: 2021-05-06 14:34:09
  * @LastEditors: longfei
  * @FilePath: \go\model\model.go
  */
@@ -16,7 +16,7 @@ import (
 	"gorm.io/gorm"
 )
 
-//这里连接数据库
+//这里连接数据库，后面可以移到专门的配置文件
 func LinkDb() *gorm.DB {
 	dsn := "gorm:123456@tcp(127.0.0.1:3306)/gorm?charset=utf8mb4&parseTime=True&loc=Local"
 	db, err := gorm.Open(mysql.Open(dsn), &gorm.Config{})
@@ -36,6 +36,7 @@ func dbConn(User, Password, Host, Db string, Port int) *gorm.DB {
 }
 
 func GetDb() (conn *gorm.DB) {
+	//这里可以切换成多数据库？
 	for {
 		conn = dbConn("gorm", "123456", "127.0.0.1", "gorm", 3306)
 		if conn != nil {
