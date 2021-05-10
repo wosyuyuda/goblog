f29fafa2d54f2d18f75f9175356ca24ac9bcabf6
longfei-jiali <itlun@qq.com>
2021-05-10 22:36:09 +0800

    添加推荐与轮播字段与数据

--- controller/index.go
+++ controller/index.go
@@ -17,12 +17,14 @@ import (
 
 type view struct {
 	gorm.Model
-	Typeid int    `gorm:"size:10"`  //分类的ID，关联
-	Title  string `gorm:"size:255"` //标题
-	Body   string //详细的内容
-	Click  int    `gorm:"size:10"`                       //点击量
-	Pic    string `gorm:"size:255"`                      //文章的缩略图
-	Tps    Tp     `json:"tps" gorm:"FOREIGNKEY:Typeid;"` //这里放分类信息types
+	Typeid  int    `gorm:"size:10"`  //分类的ID，关联
+	Title   string `gorm:"size:255"` //标题
+	Body    string //详细的内容
+	Click   int    `gorm:"size:10"`                       //点击量
+	Tuijian uint   `gorm:"size:1"`                        //是否为推荐
+	Swiper  uint   `gorm:"size:1"`                        //是否为轮播图
+	Pic     string `gorm:"size:255"`                      //文章的缩略图
+	Tps     Tp     `json:"tps" gorm:"FOREIGNKEY:Typeid;"` //这里放分类信息types
 }
 
 //这里是详情页
@@ -31,6 +33,7 @@ func GetView(c *gin.Context) {
 	db := d.LinkDb() //连接数据库模型
 	u := new(view)
 	newList := u.Findlist("0")
+	tuijian := u.Findlist("-1")
 	db.Where("id = ?", id).Find(&u)
 	db.Model(&u).Preload("Tps").Find(&u)
 	types1 := new(Tp)
@@ -39,6 +42,7 @@ func GetView(c *gin.Context) {
 		"view":    u,
 		"types":   tp,
 		"newlist": newList,
+		"tuijian": tuijian,
 	})
 }
 
@@ -48,6 +52,7 @@ func Views(c *gin.Context) {
 	v1 := new(view)
 	list := v1.Findlist(id1)
 	newList := v1.Findlist("0")
+	tuijian := v1.Findlist("-1")
 	types1 := new(Tp)
 	tp := types1.GetType("0")
 	c.HTML(http.StatusOK, "list.html", gin.H{
@@ -55,32 +60,34 @@ func Views(c *gin.Context) {
 		"typeinfo": list[0].Tps,
 		"types":    tp,
 		"newlist":  newList,
+		"tuijian":  tuijian,
 	})
-
-}
-
-//这里查询列表
-func (view) Findlist(id string) (vi []view) {
-	db := d.LinkDb() //连接数据库模型
-	if id == "0" {
-		db.Limit(10).Order("created_at desc").Preload("Tps").Find(&vi)
-		//下面是原来的
-		//db.Limit(10).Order("created_at desc").Find(&vi)
-		//db.Model(&vi).Preload("Tps").Find(&vi)
-	} else {
-		db.Where("typeid = ?", id).Limit(10).Order("created_at desc").Preload("Tps").Find(&vi)
-	}
-	return
 }
 
 //这里是首页
 func Lists1(c *gin.Context) {
 	view2 := new(view)
 	views := view2.Findlist("0")
+	tuijian := view2.Findlist("-1")
 	types1 := new(Tp)
 	tp := types1.GetType("0")
 	c.HTML(http.StatusOK, "index.html", gin.H{
-		"list":  views,
-		"types": tp,
+		"list":    views,
+		"types":   tp,
+		"tuijian": tuijian,
 	})
+}
+
+//这里查询列表,0获取全部的，-1是推荐，-2是轮播
+func (view) Findlist(id string) (vi []view) {
+	db := d.LinkDb() //连接数据库模型
+	switch id {
+	case "0":
+		db.Limit(10).Order("created_at desc").Preload("Tps").Find(&vi)
+	case "-1":
+		db.Where("tuijian = ?", 1).Limit(10).Order("created_at desc").Preload("Tps").Find(&vi)
+	default:
+		db.Where("typeid = ?", id).Limit(10).Order("created_at desc").Preload("Tps").Find(&vi)
+	}
+	return
 }
