04364cda0e9384a1fdc8ae3fedd4a46898b828d1
longfei <i@iq2.cn>
2021-05-21 14:25:25 +0800

    修改说明

--- middleware/islogin.go
+++ middleware/islogin.go
@@ -12,7 +12,7 @@ func Islogin(c *gin.Context) {
 
 	if uid == 0 || uid == "" || uid == nil { //跳转到登陆界面，这里后面应该放到中间件里面去
 		//	c.Redirect(http.StatusMovedPermanently, "/admin/login")
-		c.Redirect(http.StatusTemporaryRedirect, "/admin/login") //时间跳转到登陆页面
+		c.Redirect(http.StatusTemporaryRedirect, "/admin/login") //307跳转到登陆页面
 		c.Abort()
 		return
 	}
