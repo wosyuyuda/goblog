f6d48d109bcdcd9f89390dd33ad5902736959a72
wosylf <i@iq2.cn>
2022-11-14 20:37:49 +0800

    111

--- server/temp.go
+++ server/temp.go
@@ -2,7 +2,6 @@ package server
 
 import (
 	"bytes"
-	"fmt"
 	"html/template"
 	"io/ioutil"
 
@@ -10,14 +9,15 @@ import (
 )
 
 //方便后期整一个模板的配置功能,
-func F自己写的模板语法(c *gin.Context, temp string, db interface{}) {
+func F自己写的模板方法(c *gin.Context, temp string, db interface{}) {
+	//此处可以加一个获取配置的模板目录方法，这样就能更智能些
 	temp = "temp/xing/" + temp
 	content, err := ioutil.ReadFile(temp)
 	if err != nil {
 		RData(err, c)
 		return
 	}
-	fmt.Println("body", temp)
+	//fmt.Println("body", temp)
 	tmpl, err := template.New("tmpl").Delims("{[", "]}").Parse(string(content))
 	if err != nil {
 		RData(err, c)
