2a3aff77a7c6e802413b3982980566f5e4f80e3f
wosylf <i@iq2.cn>
2023-01-13 09:45:14 +0800

    111

--- middleware/islogin.go
+++ middleware/islogin.go
@@ -1,7 +1,6 @@
 package middleware
 
 import (
-	"goblog/config"
 	"goblog/util"
 	"net/http"
 
@@ -13,8 +12,8 @@ func Islogin(c *gin.Context) {
 	//fmt.Printf("login信息是%+v", uid)
 	if uid == 0 || uid == "" || uid == nil { //跳转到登陆界面，这里后面应该放到中间件里面去
 		//	c.Redirect(http.StatusMovedPermanently, "/admin/login")
-		cfg := config.Configv
-		c.Redirect(http.StatusTemporaryRedirect, "/admin"+cfg.GetString("adminuri")) //307跳转到登陆页面
+		//cfg := config.Configv
+		c.Redirect(http.StatusTemporaryRedirect, "/") //307跳转到登陆页面
 		c.Abort()
 		return
 	}
